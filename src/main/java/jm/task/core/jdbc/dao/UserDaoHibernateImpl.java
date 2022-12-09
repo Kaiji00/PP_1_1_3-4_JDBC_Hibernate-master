4e39a1c9376b4fb69ccf00a71dbb9678fc601576
root <stgorbissi@gmail.com>
2022-12-09 19:10:20 +0300

    refactor

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.hibernate.exception.*;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    Util util = new Util();
+    SessionFactory sessionFactory = util.getSessionFactory();
     public UserDaoHibernateImpl() {
 
     }
@@ -12,12 +19,29 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try(Session session = sessionFactory.openSession()) {
+            var transaction =  session.beginTransaction();
+            Query query = session.createNativeQuery("CREATE TABLE Users(id INT AUTO_INCREMENT" +
+                    " PRIMARY KEY," +
+                    "name VARCHAR(15) NOT NULL," +
+                    "lastName VARCHAR(20) NOT NULL," +
+                    "age TINYINT)");
+            query.executeUpdate();
+            transaction.commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            var transaction = session.beginTransaction();
+            Query query = session.createNativeQuery("DROP TABLE Users");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (SQLGrammarException e) {
+            e.printStackTrace();
 
+        }
     }
 
     @Override
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,12 +2,21 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import jm.task.core.jdbc.model.User;
 
 public class Util {
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/first_schema";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "1322";
+
+    private static SessionFactory sessionFactory;
 
     public Connection getConnection() {
         Connection connection = null;
@@ -18,5 +27,26 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/first_schema");
+            settings.put(Environment.USER, "root");
+            settings.put(Environment.PASS, "1322");
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+            settings.put(Environment.SHOW_SQL, "true");
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+        return sessionFactory;
     }
 }
