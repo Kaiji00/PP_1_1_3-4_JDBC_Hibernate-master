8084d42f3f8dce222b16e2e19e59776bacd7d280
root <stgorbissi@gmail.com>
2022-12-10 20:16:56 +0300

    refactor3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,17 +9,9 @@ public class Main {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Stanislav", "Gorbachev", (byte) 22);
-        System.out.println("User с именем " + userService.getAllUsers().get(0).getName() +
-                " добавлен в базу данных");
         userService.saveUser("Dasha", "Li", (byte) 24);
-        System.out.println("User с именем " + userService.getAllUsers().get(1).getName() +
-                " добавлен в базу данных");
         userService.saveUser("Sasha", "Ivanov", (byte) 27);
-        System.out.println("User с именем " + userService.getAllUsers().get(2).getName() +
-                " добавлен в базу данных");
         userService.saveUser("Denis", "Borodin", (byte) 25);
-        System.out.println("User с именем " + userService.getAllUsers().get(3).getName() +
-                " добавлен в базу данных");
         userService.getAllUsers().stream().forEach(System.out::println);
         userService.cleanUsersTable();
         userService.dropUsersTable();
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -14,145 +14,97 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        Connection connection = util.getConnection();
         String sql = "CREATE TABLE Users (id INT AUTO_INCREMENT PRIMARY KEY, " +
                 "name VARCHAR(15) NOT NULL, " +
                 "lastName VARCHAR(20) NOT NULL, " +
                 "age TINYINT)";
-        try (var statement = connection.createStatement()) {
-            statement.execute(sql);
-            connection.commit();
-        } catch (SQLException e) {
-            try {
+        try (Connection connection = util.getConnection()) {
+            try (var statement = connection.createStatement()) {
+                statement.execute(sql);
+                connection.commit();
+            } catch (SQLException e) {
                 connection.rollback();
-            } catch (SQLException a) {
             }
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException exc) {
-                }
-            }
+        } catch (SQLException exc) {
         }
     }
 
     public void dropUsersTable() {
-        Connection connection = util.getConnection();
         String sql = "DROP TABLE Users";
-        try (var statement = connection.createStatement()) {
-            statement.execute(sql);
-            connection.commit();
-        } catch (SQLException e) {
-            try {
+        try (Connection connection = util.getConnection()) {
+            try (var statement = connection.createStatement()) {
+                statement.execute(sql);
+                connection.commit();
+            } catch (SQLException e) {
                 connection.rollback();
-            } catch (SQLException ex) {
-            } finally {
-                if (connection != null) {
-                    try {
-                        connection.close();
-                    } catch (SQLException exc) {
-                    }
-                }
             }
+        } catch (SQLException exc) {
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        Connection connection = util.getConnection();
         String sql = "INSERT INTO Users (name, lastName, age) VALUES(?, ?, ?)";
-        try (var statement = connection.prepareStatement(sql)) {
-            statement.setString(1, name);
-            statement.setString(2, lastName);
-            statement.setByte(3, age);
-            statement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            try {
+        try (Connection connection = util.getConnection()) {
+            try (var statement = connection.prepareStatement(sql)) {
+                statement.setString(1, name);
+                statement.setString(2, lastName);
+                statement.setByte(3, age);
+                statement.executeUpdate();
+                connection.commit();
+            } catch (SQLException e) {
                 connection.rollback();
-            } catch (SQLException ex) {
-            } finally {
-                if (connection != null) {
-                    try {
-                        connection.close();
-                    } catch (SQLException exc) {
-                    }
-                }
             }
+        } catch (SQLException exc) {
         }
     }
 
     public void removeUserById(long id) {
-        Connection connection = util.getConnection();
         String sql = "DELETE FROM Users WHERE ID = ?";
-        try (var statement = connection.prepareStatement(sql)) {
-            statement.setLong(1, id);
-            statement.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            try {
+        try (Connection connection = util.getConnection()) {
+            try (var statement = connection.prepareStatement(sql)) {
+                statement.setLong(1, id);
+                statement.executeUpdate();
+                connection.commit();
+            } catch (SQLException e) {
                 connection.rollback();
-            } catch (SQLException ex) {
-            } finally {
-                if (connection != null) {
-                    try {
-                        connection.close();
-                    } catch (SQLException exc) {
-                    }
-                }
             }
+        } catch (SQLException exc) {
         }
     }
 
     public List<User> getAllUsers() {
-        Connection connection = util.getConnection();
         List<User> users = new ArrayList<>();
         String sql = "SELECT * FROM Users";
-        try (var statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                users.add(user);
-            }
-            connection.commit();
-        } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ex) {
-            } finally {
-                if (connection != null) {
-                    try {
-                        connection.close();
-                    } catch (SQLException exc) {
-                    }
+        try (Connection connection = util.getConnection()) {
+            try (var statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(sql);
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getLong("id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setLastName(resultSet.getString("lastName"));
+                    user.setAge(resultSet.getByte("age"));
+                    users.add(user);
                 }
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
             }
+        } catch (SQLException exc) {
         }
         return users;
     }
 
     public void cleanUsersTable() {
-        Connection connection = util.getConnection();
         String sql = "TRUNCATE TABLE Users";
-        try (var statement = connection.createStatement()) {
-            statement.execute(sql);
-            connection.commit();
-        } catch (SQLException e) {
-            try {
+        try (Connection connection = util.getConnection()) {
+            try (var statement = connection.createStatement()) {
+                statement.execute(sql);
+                connection.commit();
+            } catch (SQLException e) {
                 connection.rollback();
-            } catch (SQLException ex) {
-            } finally {
-                if (connection != null) {
-                    try {
-                        connection.close();
-                    } catch (SQLException exc) {
-                    }
-                }
             }
+        } catch (SQLException ex) {
         }
     }
 }
