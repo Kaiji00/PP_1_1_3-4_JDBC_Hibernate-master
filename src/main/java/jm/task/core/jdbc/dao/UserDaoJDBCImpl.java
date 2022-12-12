6c33a716e6fd6993849937e9b86f90f98ecc28f6
root <stgorbissi@gmail.com>
2022-12-12 14:08:55 +0300

    IHopeLastRefactor

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -7,7 +7,6 @@ import java.sql.*;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-
     private Util util = new Util();
 
     public UserDaoJDBCImpl() {
@@ -18,93 +17,80 @@ public class UserDaoJDBCImpl implements UserDao {
                 "name VARCHAR(15) NOT NULL, " +
                 "lastName VARCHAR(20) NOT NULL, " +
                 "age TINYINT)";
-        try (Connection connection = util.getConnection()) {
-            try (var statement = connection.createStatement()) {
-                statement.execute(sql);
-                connection.commit();
-            } catch (SQLException e) {
-                connection.rollback();
-            }
-        } catch (SQLException exc) {
+        try (Connection connection = util.getConnection();
+             var statement = util.getConnection().createStatement()) {
+            statement.execute(sql);
+            connection.commit();
+            connection.rollback();
+        } catch (SQLException e) {
         }
     }
 
     public void dropUsersTable() {
         String sql = "DROP TABLE Users";
-        try (Connection connection = util.getConnection()) {
-            try (var statement = connection.createStatement()) {
-                statement.execute(sql);
-                connection.commit();
-            } catch (SQLException e) {
-                connection.rollback();
-            }
-        } catch (SQLException exc) {
+        try (Connection connection = util.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+            connection.commit();
+            connection.rollback();
+        } catch (SQLException e) {
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
         String sql = "INSERT INTO Users (name, lastName, age) VALUES(?, ?, ?)";
-        try (Connection connection = util.getConnection()) {
-            try (var statement = connection.prepareStatement(sql)) {
-                statement.setString(1, name);
-                statement.setString(2, lastName);
-                statement.setByte(3, age);
-                statement.executeUpdate();
-                connection.commit();
-            } catch (SQLException e) {
-                connection.rollback();
-            }
-        } catch (SQLException exc) {
+        try (Connection connection = util.getConnection();
+             var statement = connection.prepareStatement(sql)) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+            connection.commit();
+            connection.rollback();
+        } catch (SQLException ignored) {
         }
     }
 
     public void removeUserById(long id) {
         String sql = "DELETE FROM Users WHERE ID = ?";
-        try (Connection connection = util.getConnection()) {
-            try (var statement = connection.prepareStatement(sql)) {
-                statement.setLong(1, id);
-                statement.executeUpdate();
-                connection.commit();
-            } catch (SQLException e) {
-                connection.rollback();
-            }
-        } catch (SQLException exc) {
+        try (Connection connection = util.getConnection();
+             var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+            connection.commit();
+        } catch (SQLException ignored) {
         }
     }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
         String sql = "SELECT * FROM Users";
-        try (Connection connection = util.getConnection()) {
-            try (var statement = connection.createStatement()) {
-                ResultSet resultSet = statement.executeQuery(sql);
-                while (resultSet.next()) {
-                    User user = new User();
-                    user.setId(resultSet.getLong("id"));
-                    user.setName(resultSet.getString("name"));
-                    user.setLastName(resultSet.getString("lastName"));
-                    user.setAge(resultSet.getByte("age"));
-                    users.add(user);
-                }
-                connection.commit();
-            } catch (SQLException e) {
-                connection.rollback();
+        try (Connection connection = util.getConnection();
+             var statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
             }
-        } catch (SQLException exc) {
+            connection.commit();
+            connection.rollback();
+        } catch (SQLException ignored) {
         }
         return users;
     }
 
     public void cleanUsersTable() {
         String sql = "TRUNCATE TABLE Users";
-        try (Connection connection = util.getConnection()) {
-            try (var statement = connection.createStatement()) {
-                statement.execute(sql);
-                connection.commit();
-            } catch (SQLException e) {
-                connection.rollback();
-            }
-        } catch (SQLException ex) {
+        try (Connection connection = util.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+            connection.commit();
+            connection.rollback();
+        } catch (SQLException ignored) {
         }
     }
 }
